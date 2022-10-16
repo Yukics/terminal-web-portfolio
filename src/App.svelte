aeb0855848ddcbe56ba2116a76899617b8322c18
Yukics <alejomorell@gmail.com>
2022-10-16 15:12:24 +0200

    st dinamically render components

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import PS1 from "./components/PS1.svelte";
   import LS from "./components/LS.svelte";
+  
   let command:string = "";
 
   function enterCommand(){
     console.log(command)
+    switch(command){
+      case "ls":
+        break;
+      case "cv":
+        break;
+      case "help":
+        break;
+      default:
+        break;
+    }
+    command = "";
   }
   function catCV(){
         // TODO Print my cv
@@ -18,7 +30,7 @@
   <PS1 executedCommand={"ls"}/>
   <LS catCV={catCV()} catContact={catContact()}/>
   <PS1 bind:command enterKey={enterCommand}/>
-  <!-- <h1>IT IS: {command}</h1> -->
+
 </body>
 
 <style>
